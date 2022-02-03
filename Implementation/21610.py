2732281b4f42762e6b31eb86b2d54e1e375d4384
jinsol0330 <jinsol0330@naver.com>
2022-02-03 14:36:20 +0900

    (220202) solved algorithm

--- Implementation/21610.py
+++ Implementation/21610.py
@@ -20,7 +20,7 @@ for d, s in move_info:
     for idx, cloud in enumerate(cloud_loc):
         x, y = cloud[0], cloud[1]
         cloud_loc[idx][0] = (x + directions[d-1][0] * s) % N
-        cloud_loc[idx][1] = (y + directions[d-1][1] * s) % N
+        cloud_loc[idx][1] = (N + y + directions[d-1][1] * s) % N
         
     # 뒤에 물의 양을 2 줄일 때 구름 위치를 제외하고 줄이기 위해 True로 변경
     visited = [[False] * N for _ in range(N)]
@@ -28,7 +28,6 @@ for d, s in move_info:
         # 구름이 있는 칸의 바구니에 저장된 물의 양 1 증가
         board[x][y] += 1
         visited[x][y] = True
-
     # step2 : 물복사버그 마법 시전
     # 현재 이동 후 구름의 위치가 저장되어있음
     for x, y in cloud_loc:
@@ -36,12 +35,11 @@ for d, s in move_info:
         # 대각선 방향으로 거리가 1칸
         case = [[-1, -1], [-1, 1], [1, 1], [1, -1]]
         for c in range(4):
-            newx, newy = x + case[c][0], y + case[c][1]
+            newx, newy = x + case[c][0], x + case[c][1]
             # 대각선 방향에 물이 있는 바구니의 수 만큼 물의 양 증가
             if 0 <= newx < N and 0<= newy < N and board[newx][newy] >= 1:
                 cnt += 1
         board[x][y] += cnt
-    print(board)
     new_cloud_loc = []
     # step3 : 물의 양이 2 이상인 칸에 구름이 생기고 물의 양 2만큼 줄이기
     for row in range(N):
